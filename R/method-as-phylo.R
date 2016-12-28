268b8f7d6e99a2e76d0fca7e1bf4643c997b4030
guangchuang yu <guangchuangyu@gamil.com>
2016-12-28 21:41:01 +0800

    reexport as.phylo

--- R/method-as-phylo.R
+++ R/method-as-phylo.R
@@ -1,4 +1,8 @@
 ##' @importFrom ape as.phylo
+##' @export
+ape::as.phylo
+
+
 ##' @method as.phylo treedata
 ##' @export
 as.phylo.treedata <- function(x, ...) {
@@ -8,20 +12,20 @@ as.phylo.treedata <- function(x, ...) {
 
 ##' @method as.phylo phylo4
 ##' @export
-as.phylo.phylo4 <- function(phylo4) {
-    edge <- phylo4@edge
+as.phylo.phylo4 <- function(x, ...) {
+    edge <- x@edge
     edge <- edge[edge[,1] != 0, ]
-    edge.length <- phylo4@edge.length
+    edge.length <- x@edge.length
     edge.length <- edge.length[!is.na(edge.length)]
     tip.id <- sort(setdiff(edge[,2], edge[,1]))
-    tip.label <- phylo4@label[tip.id]
+    tip.label <- x@label[tip.id]
     phylo <- list(edge = edge,
                   edge.length = edge.length,
                   tip.label = tip.label)
 
     node.id <- sort(unique(edge[,1]))
     node.id <- node.id[node.id != 0]
-    node.label <- phylo4@label[node.id]
+    node.label <- x@label[node.id]
     if (!all(is.na(node.label))) {
         phylo$node.label <- node.label
     }
