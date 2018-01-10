0a199e7f52c87cf4a5cebd7a8900978dd628865f
guangchuang yu <guangchuangyu@gamil.com>
2018-01-10 19:36:15 +0800

    avoid long code line

--- tests/testthat/test-r8s.R
+++ tests/testthat/test-r8s.R
@@ -1,7 +1,7 @@
 context("r8s")
 
-
-r8s <- read.r8s(system.file("extdata/r8s", "H3_r8s_output.log", package="treeio"))
+file <- system.file("extdata/r8s", "H3_r8s_output.log", package="treeio")
+r8s <- read.r8s(file)
 
 test_that("parsing r8s log file", {
     expect_true(all(c("TREE", "RATO", "PHYLO") %in% names(r8s)))
