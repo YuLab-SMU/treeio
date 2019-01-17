8773db62b98795ad4c99dbdebf02a65ba0d5bbda
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-01-17 13:41:03 +0900

    Rename root() to reroot()
    This function extends phytools::reroot(), not ape::root().
    The original naming was very confusing and caused conflicts with
    ape::root() on some occasions.

--- R/method-reroot.R
+++ R/method-reroot.R
@@ -36,26 +36,29 @@ reroot_node_mapping <- function(tree, tree2) {
 
 ##' re-root a tree
 ##'
-##' 
-##' @title root
-##' @rdname root-method
+##'
+##' @title reroot
+##' @rdname reroot-method
 ##' @param object tree object
 ##' @param node node to reroot
 ##' @param ... additional parameters
 ##' @return rerooted tree
-##' @importFrom ape root
-##' @method root phylo
-##' @export
 ##' @author Guangchuang Yu
-root.phylo <- function(object, node, ...) {
+##' @export
+reroot <- function(object, node, ...) UseMethod("reroot")
+
+##' @rdname reroot-method
+##' @method reroot phylo
+##' @export
+reroot.phylo <- function(object, node, ...) {
     pos <- 0.5* object$edge.length[which(object$edge[,2] == node)]
-    
+
     ## @importFrom phytools reroot
     phytools <- "phytools"
     require(phytools, character.only = TRUE, quietly = TRUE)
-    
+
     phytools_reroot <- eval(parse(text="phytools::reroot"))
-    
+
     tree <- phytools_reroot(object, node, pos)
     attr(tree, "reroot") <- TRUE
     node_map <- reroot_node_mapping(object, tree)
@@ -64,10 +67,10 @@ root.phylo <- function(object, node, ...) {
 }
 
 
-##' @rdname root-method
-##' @method root treedata
+##' @rdname reroot-method
+##' @method reroot treedata
 ##' @export
-root.treedata <- function(object, node, ...) {
+reroot.treedata <- function(object, node, ...) {
                                         # warning message
     message("The use of this method may cause some node data to become incorrect (e.g. bootstrap values).")
 
@@ -81,34 +84,34 @@ root.treedata <- function(object, node, ...) {
     if (is.null(tree$node.label)) {
         tree$node.label <- as.character((1:tree$Nnode) + Ntip(tree))
     }
-    
+
                                         # reroot tree
-    tree <- root(tree, node, ...)
+    tree <- reroot(tree, node, ...)
     newobject@phylo <- tree
 
                                         # update node numbers in data
     n.tips <- Ntip(tree)
     node_map<- attr(tree, "node_map")
-    
+
     update_data <- function(data, node_map) {
         newdata <- data
         newdata[match(node_map$from, data$node), 'node'] <- node_map$to
-        
+
                                         # clear root data
         root <- newdata$node == (n.tips + 1)
         newdata[root,] <- NA
         newdata[root,'node'] <- n.tips + 1
-        
+
         return(newdata)
     }
-    
+
     if (nrow(newobject@data) > 0) {
         newobject@data <- update_data(object@data, node_map)
     }
-    
+
     if (nrow(object@extraInfo) > 0) {
         newobject@extraInfo <- update_data(object@extraInfo, node_map)
     }
-    
+
     return(newobject)
 }
