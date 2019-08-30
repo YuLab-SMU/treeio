ca717f2ff6872418c8ceeecb7e4e101474cc4895
Guangchuang Yu <guangchuangyu@gmail.com>
2019-08-30 12:59:06 +0800

    default value of MRCA(.node2 =NULL)

--- R/method-MRCA.R
+++ R/method-MRCA.R
@@ -2,12 +2,12 @@
 ##' @importFrom tidytree as_tibble
 ##' @method MRCA phylo
 ##' @export
-MRCA.phylo <- function(.data, .node1, .node2, ...) {
+MRCA.phylo <- function(.data, .node1, .node2 = NULL, ...) {
     MRCA(as_tibble(.data), .node1, .node2, ...)[["node"]]
 }
 
 ##' @method MRCA treedata
 ##' @export
-MRCA.treedata <- function(.data, .node1, .node2, ...) {
+MRCA.treedata <- function(.data, .node1, .node2 = NULL, ...) {
     MRCA.phylo(as.phylo(.data), .node1, .node2, ...)
 }
