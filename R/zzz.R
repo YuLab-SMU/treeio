e5dd44b16a86844c513e027aa2161f6e93f98bd4
Guangchuang Yu <guangchuangyu@gmail.com>
2020-11-13 22:28:42 +0800

    update url

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@ globalVariables(".")
 .onAttach <- function(libname, pkgname) {
     pkgVersion <- packageDescription(pkgname, fields="Version")
     msg <- paste0(pkgname, " v", pkgVersion, "  ",
-                  "For help: https://yulab-smu.github.io/treedata-book/", "\n\n")
+                  "For help: https://yulab-smu.top/treedata-book/", "\n\n")
 
     citation <- paste0("If you use ", pkgname, " in published research, please cite:\n\n",
                        "LG Wang, TTY Lam, S Xu, Z Dai, L Zhou, T Feng, P Guo, CW Dunn, BR Jones, T Bradley, H Zhu, Y Guan, Y Jiang, G Yu. ",
