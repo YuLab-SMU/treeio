69263feab03da476ffee23257b87f6e163e53726
xiangpin <xshuangbin@163.com>
2021-06-08 11:38:39 +0800

    fix the check of ultrametric

--- R/MCMCTree.R
+++ R/MCMCTree.R
@@ -2,7 +2,7 @@
 ##'
 ##' @title read.mcmctree
 ##' @param file the output tree file of MCMCTree
-##' @param as.ultrametric logical whether convert the tree 
+##' @param force.ultrametric logical whether convert the tree 
 ##' to be ultrametric, if it is not ultrametric, default is FALSE.
 ##' When the tree is ultrametric, branch times will be calculated 
 ##' automatically. 
@@ -12,7 +12,7 @@
 ##' file <- system.file("extdata/MCMCTree", "mcmctree_output.tree", package="treeio")
 ##' tr <- read.mcmctree(file)
 ##' tr
-read.mcmctree <- function(file, as.ultrametric = FALSE){
+read.mcmctree <- function(file, force.ultrametric = FALSE){
     text <- readLines(file)
     ind <- grep("^.*tree.*=.*", text, ignore.case=TRUE)
     text[ind] <- gsub("^.*TREE", "TREE", text[ind], ignore.case=TRUE)
@@ -24,14 +24,14 @@ read.mcmctree <- function(file, as.ultrametric = FALSE){
         obj@file <- filename(file)
         obj <- add.branch.time.mcmctree(
                                         obj=obj, 
-                                        as.ultrametric = as.ultrametric, 
+                                        force.ultrametric = force.ultrametric, 
                                         )
     }else{
         for (i in seq_len(length(obj))){
             obj[[i]]@file <- filename(file)
             obj[[i]] <- add.branch.time.mcmctree(
                                         obj=obj[[i]], 
-                                        as.ultrametric = as.ultrametric
+                                        force.ultrametric = force.ultrametric
                                         )
         }
     }
@@ -40,10 +40,11 @@ read.mcmctree <- function(file, as.ultrametric = FALSE){
 
 #' @importFrom ape branching.times
 #' @importFrom ape is.ultrametric
-add.branch.time.mcmctree <- function(obj, as.ultrametric=FALSE, ...){
-    flag_ultrametric <- !is.ultrametric(obj@phylo, option=2) || !is.ultrametric(obj@phylo)
-    if (as.ultrametric && flag_ultrametric){
-        obj <- as.ultrametric(obj)
+add.branch.time.mcmctree <- function(obj, force.ultrametric=FALSE, ...){
+    flag_ultrametric <- is.ultrametric(obj@phylo, option=2) || is.ultrametric(obj@phylo)
+    if (force.ultrametric && flag_ultrametric){
+        message("This tree is not ultrametric, and you has set force.ultrametric to TRUE, so the tree will be convert to ultrametric automatically!")
+        obj <- force.ultrametric(obj)
     }
     if (flag_ultrametric){
         xx <- branching.times(obj@phylo)
