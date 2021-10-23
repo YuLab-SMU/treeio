8150859063200fe0db9d213862b8c9868da653ed
Guangchuang Yu <guangchuangyu@gmail.com>
2021-10-23 10:46:32 +0800

    Update RAxML.R

--- R/RAxML.R
+++ R/RAxML.R
@@ -14,8 +14,9 @@ read.raxml <- function(file) {
     tree_text <- gsub('(:[0-9\\.eE+\\-]+)\\[(\\d+)\\]', '\\@\\2\\1', tree.text)
     phylo <- read.tree(text=tree_text)
     if(any(grepl('@', phylo$node.label))) {
-        bootstrap <- as.numeric(gsub("[^@]*@(\\d+)", "\\1", phylo$node.label)) %>%
-                     suppressWarnings()
+        bootstrap <- suppressWarnings(
+            as.numeric(gsub("[^@]*@(\\d+)", "\\1", phylo$node.label)) 
+                   )
         phylo$node.label <- gsub("@\\d+", "", phylo$node.label)
     }
 
