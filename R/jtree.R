e91c81810808f0b5a617411e0f3675454ac725e3
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2023-11-22 10:15:05 +1300

    fix jtree

--- R/jtree.R
+++ R/jtree.R
@@ -12,7 +12,7 @@ read.jtree <- function(file) {
     phylo <- jplace_treetext_to_phylo(jtree$tree)
     edgeNum.df <- attr(phylo, "edgeNum")
     d <- merge(edgeNum.df, jtree$data, by.x = "edgeNum", by.y = "edge_num") %>%
-        as_tibble %>% select(- .data$edgeNum)
+        as_tibble %>% select(- "edgeNum")
     new("treedata",
         treetext = jtree$tree,
         phylo = phylo,
@@ -61,19 +61,21 @@ write.jtree <- function(treedata, file = "") {
             sub("@@(\\d+)", "{\\1}", .)
     }
 
-    cat("{\n", file = file)
-
-    cat(paste0('\t"tree": "', tree_text, '",\n'), file = file, append = TRUE)
-    cat('\t"data":', file = file, append = TRUE)
-
+    buffer <- c("{\n")
+    buffer <- c(buffer, paste0('\t"tree": "', tree_text, '",\n'))
+    buffer <- c(buffer, '\t"data":')
+    
     data <- get_tree_data(treedata)
     cn <- colnames(data)
     data <- data[, c("node", cn[cn != "node"])]
-    data <- rename(data, edge_num=.data$node)
-
-    cat(toJSON(data, pretty=TRUE), file = file, append = TRUE)
+    data <- rename(data, edge_num="node")
+    
+    buffer <- c(buffer, toJSON(data, pretty=TRUE))
     metainfo <- ',\n\t"metadata": {"info": "R-package treeio", '
     metainfo <- paste0(metainfo, '"data": ', paste0('"', date(), '"'), '}\n')
-    cat(metainfo, file = file, append = TRUE)
-    cat("}\n", file = file, append = TRUE)
+    buffer <- c(buffer, metainfo)
+    buffer <- c(buffer, "}\n")
+    
+    writeLines(buffer, file)
+    return(buffer)
 }
